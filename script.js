b28d2db3e6405d3c5653dc77f23a435a7bcb9d5c
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2024-12-14 10:26:34 -0800

    Added placeholder API

--- script.js
+++ script.js
@@ -2,33 +2,54 @@ const colorPicker = document.getElementById('color-picker')
 const colorChosenEl = document.getElementById('color-chosen')
 const virtualNav = document.getElementById('virtual-nav') 
 const virtualMain = document.getElementById('virtual-main')
-const virtualBubble = document.getElementById('virtual-bubble')
+const virtualBubble = document.getElementById('virtual-content')
 const virtualText = document.getElementById('virtual-text')
+// const placeholderPosts = document.querySelectorAll('.placeholder-post')
 
 colorPicker.addEventListener("input", getColor)
 colorPicker.addEventListener("change", getColor)
 
-// function updateColorEl() {
-//     colorChosenEl.innerText = getColor()
-// }
-
 function getColor(event) {
     let targetValue = event.target.value.slice(1) //Call the input (color selector), and slice the '#' off the value string
     colorChosenEl.innerText = event.target.value //Display the picked color hex value in #color-chosen span
-    
+    // getPlaceholders()
+
     fetch (`https://www.thecolorapi.com/scheme?hex=${targetValue}&count=36`)
     .then(res => res.json())
     .then(data => {
         const fullColorHexArray = Object.values(data.colors) //After calling the api, we get the color hex values across all the calls
         .map(color => hexValue = color.hex.value) //Then create the array out of only the values, the only thing used in the app finishing the const
 
-        console.log(Object.values(data.colors[18]))
-        const colorNameArray = Object.values(data.colors[18].name)
+        const colorNameArray = Object.values(data.colors[18].name) //The chosen color appears in the middle of the API call, we pick it out and give it the name displayed here
         colorChosenEl.innerText = `#${targetValue} => ${colorNameArray[0]}`
-        virtualNav.style.backgroundColor = fullColorHexArray[6]
+        virtualNav.style.backgroundColor = fullColorHexArray[3]
         virtualNav.style.color = fullColorHexArray[30]
-        virtualMain.style.backgroundColor = fullColorHexArray[17]
+        virtualMain.style.backgroundColor = fullColorHexArray[18]
         virtualBubble.style.backgroundColor = fullColorHexArray[35]
         virtualText.style.color = fullColorHexArray[0]
+        // placeholderPosts.style.border = `2px solid black`
    })
+
+   
 }
+
+function getPlaceholders(number) {
+    number ? number = number : number = 5
+
+    fetch ('https://jsonplaceholder.typicode.com/posts/')
+    .then(res => res.json())
+    .then(data => {
+        const postsArray = data.slice(0, 3)
+
+        virtualText.innerHTML = postsArray.map(post => 
+            `<article class="placeholder-post">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            <hr>
+            </article>`
+        ).join('')
+    })
+
+}
+
+getPlaceholders()
