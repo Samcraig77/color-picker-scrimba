65a910d281efcfe022954468e3b5fda14a9d1f19
Samcraig77 <SAMCRAIG70@GMAIL.COM>
2024-12-15 08:01:47 -0800

    Placeholder input amount functionality  added

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const virtualNav = document.getElementById('virtual-nav')
 const virtualMain = document.getElementById('virtual-main')
 const virtualBubble = document.getElementById('virtual-content')
 const virtualText = document.getElementById('virtual-text')
+const navColor = document.getElementById('nav-color')
+const placeholderNumber = document.getElementById('placeholder-posts-number')
 // const placeholderPosts = document.querySelectorAll('.placeholder-post')
 
 colorPicker.addEventListener("input", getColor)
 colorPicker.addEventListener("change", getColor)
+placeholderNumber.addEventListener("input", (e => {
+    getPlaceholders(e.target.value)
+}))
+
 
 function getColor(event) {
     let targetValue = event.target.value.slice(1) //Call the input (color selector), and slice the '#' off the value string
@@ -28,8 +34,6 @@ function getColor(event) {
         virtualBubble.style.backgroundColor = fullColorHexArray[35]
         virtualText.style.color = fullColorHexArray[0]
     })
-
-   
 }
 
 function getPlaceholders(number) {
@@ -38,10 +42,8 @@ function getPlaceholders(number) {
     fetch ('https://jsonplaceholder.typicode.com/posts/')
     .then(res => res.json())
     .then(data => {
-        const postsArray = data.slice(0, 3)
+        const postsArray = data.slice(0, `${number}`)
         
-        
-
         virtualText.innerHTML = postsArray.map(post => 
             `<article class="placeholder-post">
             <h2>${post.title}</h2>
@@ -50,7 +52,6 @@ function getPlaceholders(number) {
             </article>`
         ).join('')
     })
-
 }
 
-getPlaceholders()
+getPlaceholders(5)
